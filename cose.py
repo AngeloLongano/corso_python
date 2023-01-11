53e856af4e35ecc9c3ab14382aac7be4dbc91775
Angelo Longano <angelo.longano@gmail.com>
2023-01-11 02:23:06 +0100

    Caricata lezione 6

--- cose.py
+++ cose.py
@@ -1,35 +1,12 @@
-class Stack:
+import unittest
 
-    def __init__(self):
-        self.values=[]
-    def push(self,x):
-        self.values.append(x)
-    def pop(self):
-        self.values.pop()
-    def is_empty(self):
-        return self.values.__len__()==0
-
-a = Stack()
-print(a.values)
-print(a.is_empty())
-a.push(10)
-a.push(1)
-a.push(3)
-a.pop()
-print(a.values)
-print(a.is_empty())
+value = True  # False
 
 
-class Ciao:
-    def __init__(self, a,b):
-        self.a = a
-        self.b = b
-    def __getattribute__(self, item):
-        print("entro in getattribute")
-        if item == "a" :
-            return super().__getattribute__(item)
-        else:
-            return "paperino"
-    def __getattr__(self, item):
-        return "giumon"
+class Foo(unittest.TestCase):
+    # Deve ritornare True o False
+    def test(self):
+        self.assertTrue(value)
 
+
+unittest.main()
